1d3fa3e74380f863eace82d0e2523ab7ee67cb14
ren <2336252728@qq.com>
2025-08-24 10:09:38 +0800

    kv键值存储微服务搭建

---
+++ xiaohongshu-kv/xiaohongshu-kv-biz/src/main/java/com/zry/xiaohongshu/kv/biz/XiaohongshuKVBizApplication.java
@@ -0,0 +1,11 @@
+package com.zry.xiaohongshu.kv.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuKVBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuKVBizApplication.class, args);
+    }
+}
