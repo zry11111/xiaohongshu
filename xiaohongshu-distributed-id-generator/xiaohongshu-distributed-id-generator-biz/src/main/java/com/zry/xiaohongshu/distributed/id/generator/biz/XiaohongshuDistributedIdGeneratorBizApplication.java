9001f516b3ec9eecb6b1ef883a770d42d0f53e2b
ren <2336252728@qq.com>
2025-08-26 18:10:24 +0800

    搭建分布式ID生成微服务

---
+++ xiaohongshu-distributed-id-generator/xiaohongshu-distributed-id-generator-biz/src/main/java/com/zry/xiaohongshu/distributed/id/generator/biz/XiaohongshuDistributedIdGeneratorBizApplication.java
@@ -0,0 +1,12 @@
+package com.zry.xiaohongshu.distributed.id.generator.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class XiaohongshuDistributedIdGeneratorBizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuDistributedIdGeneratorBizApplication.class, args);
+    }
+}
