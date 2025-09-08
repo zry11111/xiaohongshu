18c1b513e586f270d46984af36def879ac06ea93
ren <2336252728@qq.com>
2025-09-08 17:01:14 +0800

    初始化搜索服务

---
+++ xiaohongshu-search/src/main/java/com/zry/xiaohongshu/search/XiaohongshuSearchApplication.java
@@ -0,0 +1,11 @@
+package com.zry.xiaohongshu.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuSearchApplication.class, args);
+    }
+}
