263d37d1b9d939e6d73c6dc131afdf61f98065b7
ren <2336252728@qq.com>
2025-09-11 14:38:28 +0800

    feat.获取热更新词典接口

---
+++ xiaohongshu-search/src/main/java/com/zry/xiaohongshu/search/service/ExtDictService.java
@@ -0,0 +1,11 @@
+package com.zry.xiaohongshu.search.service;
+
+import org.springframework.http.ResponseEntity;
+
+public interface ExtDictService {
+    /**
+     * 获取热更新词典
+     * @return
+     */
+    ResponseEntity<String> getHotUpdateExtDict();
+}
